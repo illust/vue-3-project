21eb8d6481fa42a59708dd13b3692cef75df41d7
svenwong <illust0130@foxmail.com>
2022-02-22 23:26:24 +0800

    add two init projects

---
+++ composition-api/src/components/CourseGoals.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <goals-list></goals-list>
+      <add-goal></add-goal>
+  </div>
+</template>
+
+<script>
+
+import GoalsList from "./GoalsList.vue";
+import AddGoal from "./AddGoal.vue";
+
+export default {
+    components:{
+        GoalsList,
+        AddGoal
+    },
+    setup(){
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
